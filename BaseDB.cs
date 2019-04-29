bf59c033e0ea460f3b41c5695e2ddc41940c6e7c
dagnais <40569950+dagnais@users.noreply.github.com>
2019-04-29 19:59:12 -0300

    Ecowell
    App con coordenadas gps

---
+++ BaseDB.cs
@@ -0,0 +1,183 @@
+﻿/*
+Titulo: "Ecowell"
+Hecho en el año:2018 
+-----
+Title: "Ecowell"
+Made in the year: 2018
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using System.IO;
+using System;
+
+public class BaseDB{
+    private string textJson; //Como .json es un archivo de texto aqui se almacena el contenido.
+    public JSONNode node; // Utilizamos esta variable para leer como base de datos al archivo .json.
+
+    //Establecemos el contenido del .json actual.
+    public void SetCurretText(string txt)
+    {
+        textJson = txt;
+        node= JSON.Parse(textJson);
+    }
+
+    //Establecemos el contenido del .json como nodo.
+    public void SetCurrentNode(JSONNode currentNode)
+    {
+        node = currentNode;
+    }
+
+    //Guardamos los cambios en la base de datos en la ubicacion especificada
+    public void Save(string pathFile)
+    {
+        //Ejemplo de ubicacion.
+        //"C:/DB/Clientes.json"
+        File.WriteAllText(pathFile, node.ToString());
+    }
+
+    //Cargamos los datos del archivo .json desde la ubicacion especificada.
+    public void Load(string pathFile)
+    {
+        //Ejemplo de ubicacion.
+        //"C:/DB/Clientes.json"
+        //pathFile= "D:/Animals.json";
+        node = JSON.Parse(File.ReadAllText(pathFile));
+        Debug.Log("node: " + node.ToString());
+        //Descomenta la linea de arriba para ver el contenido que posee la carga de datos.
+    }
+
+    //Obtiene la cantidad de entradas que posee la base de datos.
+    public int GetAmount()
+    {
+        //En products dejamos una sugerencia de estructura para hacer la BD .json
+        //Si la has seguido podras usar esta funcion en caso contrado deberas modificar el codigo o no usarla.
+        return node["Cantidad"].AsInt;
+    }
+
+    //Ingresamos los datos a ser guardados en la base de datos.
+    public void SaveNewItem(string dataBase, string pathFile,string search, string[] fields, string[] values)
+    {
+        //Argumentos: nombre de la base de datos, ubicacion , idunico o encabezado de item, campos que contiene 
+        //el item , datos a guardar.
+        int count = GetAmount();
+        //Advertencia para que este codigo funcione debes haber seguido la sugerencia dejada en
+        //Products, o tener como primer campo uno llamado index.
+        //Este sirve para identificar la fila en la que se encuentra el item.
+        node[dataBase][search]["index"] = count.ToString();
+        for (int i = 0; i < fields.Length; i++)
+        {
+            node[dataBase][count][fields[i]] =values[i];
+        }
+        count++;
+        node["Cantidad"] = count.ToString();
+        Save(pathFile);
+    }
+
+    //Borra una item en la base de datos.
+    public void RemoveItem(string dataBase, string pathFile, string seach)
+    {
+        //Argumentos: nombre de BD, ubicacion , id unico para identificar el item a borrar.
+        int codeReplace = node[dataBase][seach]["index"].AsInt;
+        if (codeReplace > 0)
+        {
+            node[dataBase].Remove(seach);
+
+            int count = GetAmount(); 
+
+            count--;
+
+            node["Cantidad"] = count.ToString();
+
+            for (int i = codeReplace; i < node[dataBase].Count; i++)
+            {
+                node[dataBase][i]["index"] = i.ToString();
+            }
+
+            Save(pathFile);
+        }
+    }
+
+    //Devuelve una serie de strings resultado de una busqueda.
+    public string[] ValidateItem(string dataBase, string search, string[] fields)
+    {
+        //Argumentos: nombre DB, ubicacion , id de busqueda, nombre de los campos en donde se busca.
+        int count;
+        string[] values;
+        //Advertencia para que este codigo funcione debes haber seguido la sugerencia dejada en
+        //Products, o tener como primer campo uno llamado index.
+        //Este sirve para identificar la fila en la que se encuentra el item.
+        count = node[dataBase][search]["index"].AsInt;
+
+        if (count > 0)
+        {
+            values = new string[fields.Length];
+            for (int i = 0; i < fields.Length; i++)
+            {
+                values[i] = node[dataBase][count][fields[i]].Value;
+            }
+            return values;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    //Realiza una busqueda en toda la base de datos.
+    public string[] SearchInDBItem(int i, string dataBase, string[] fields)
+    {
+        //Argumentos: indicador para utilizarse en estructura repetitiva como for.
+        // nombre de la base de datos, nombres de los campos a buscar.
+        string[] values;
+
+        values = new string[fields.Length];
+        for (int j = 0; j < fields.Length; j++)
+        {
+            //Debug.Log("db: "+dataBase +" i: "+i + " fl: "+fields[j]);
+            values[j] = node[dataBase][i][fields[j]].Value;
+        }
+        return values;
+    }
+
+    //Realiza una busqueda en toda la base de datos sobre el campo "nombre".
+    public string[] GetNamesItem(string dataBase)
+    {
+        //Argumentos: nombre de la base de datos.
+        string[] names = new string[node[dataBase].Count - 1];
+        for (int i = 1; i < node[dataBase].Count; i++)
+        {
+            names[i - 1] = node[dataBase][i]["nombre"].Value;
+        }
+        return names;
+    }
+
+    public string[] SearchItem(string dataBase, string search, string[] fields)
+    {
+        //Argumentos: nombre DB, ubicacion , id de busqueda, nombre de los campos en donde se busca.
+
+        string[] values;
+        //Advertencia para que este codigo funcione debes haber seguido la sugerencia dejada en
+        //Products, o tener como primer campo uno llamado index.
+        //Este sirve para identificar la fila en la que se encuentra el item.
+            values = new string[fields.Length];
+            for (int i = 0; i < fields.Length; i++)
+            {
+                values[i] = node[dataBase][search][fields[i]].Value;
+            }
+            return values;
+    }
+
+    public string[] GetIndexByOrder(string dataBase)
+    {
+        string[] values;
+        values = new string[GetAmount()];
+        for (int i = 0; i < values.Length; i++)
+        {
+            //Debug.Log(node[dataBase][i]["index"].Value);
+            values[i] = node[dataBase][i]["index"].Value;
+        }
+        return values;
+    }
+}
---
+++ MainController.cs
@@ -0,0 +1,229 @@
+﻿/*
+Titulo: "Ecowell"
+Hecho en el año:2018 
+-----
+Title: "Ecowell"
+Made in the year: 2018
+*/
+using UnityEngine;
+using AnimalsDetails;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MainController : MonoBehaviour {
+    #region UI
+    public bool isLerp;
+    public Vector3 endMarker;
+
+    private float lerpCount;
+
+    [Header("Icons")]
+    public Sprite activeSearchMarker;
+    public Sprite normalMarker, farmerMarker,
+        alertMarker, transMarker, outCorralMarker;
+
+    [Header("UI Elements")]
+    public GameObject[] menuButtons;
+    public GameObject mapObject, panelSearch, contentViewMap, searchBtn, cancelSearchBtn;
+    public InputField latObj, lonObj, searchField;
+    public Button zoomInBtn, zoomOutBtn;
+    Vector3 contentOriginPos;
+    string searchText;
+
+    #endregion
+    [Header("Others")]
+    int zoomLevel=5;
+    GoogleMap googleMap;
+    public GoogleMapLocation country;
+
+    public Text pathDB;
+    [HideInInspector]
+    public Animals animalsDB;
+
+    public List<MarkerController> mk;
+    int mkI;
+
+    void Start() {
+
+        if (mapObject != null)
+            googleMap = mapObject.GetComponent<GoogleMap>();
+        else
+            Debug.LogError("Mapa no asignado");
+
+
+        animalsDB = new Animals();
+        if (animalsDB == null)
+            Debug.LogError("Variable vacia");
+
+        if (pathDB == null)
+            Debug.LogError("pathDB no esta asignada con un objeto de tipo UI Text");
+        pathDB.text = "Path: " + animalsDB.pathAnimals;
+
+        AutosearchCountry();
+
+        contentOriginPos = contentViewMap.transform.localPosition;
+    }
+
+    void Update() {
+        if (isLerp)
+            LerpContent();
+    }
+
+    void StartLerp()
+    {
+        lerpCount = 0;
+    }
+
+    void LerpContent()
+    {
+        contentViewMap.transform.localPosition = Vector3.Lerp(contentViewMap.transform.localPosition,
+            endMarker, 0.01f);
+        Debug.Log(endMarker);
+        if (lerpCount < 2f)
+        {
+            lerpCount += 0.01f;
+        }
+        else
+        {
+            isLerp = false;
+        } 
+    }
+
+    public void SearchLocationCountry()
+    {
+        panelSearch.SetActive(false);
+        mapObject.SetActive(true);
+        googleMap.centerLocation.latitude = double.Parse(latObj.text);
+        googleMap.centerLocation.longitude = double.Parse(lonObj.text);
+        GetAmmountMarkers();
+        googleMap.Refresh();
+        country = googleMap.centerLocation;
+    }
+
+    void AutosearchCountry()
+    {
+        string[] st = new string[2];
+        st = animalsDB.SearchInDBItem(0, animalsDB.dbAnimals, animalsDB.fieldsAnimal);
+        mapObject.SetActive(true);
+        googleMap.centerLocation = new GoogleMapLocation(string.Empty, double.Parse(st[0]), double.Parse(st[1]));
+        googleMap.Refresh();
+    }
+    void GetAmmountMarkers()
+    {
+        string[] st = new string[2];
+        googleMap.markers= new GoogleMapMarker[animalsDB.GetAmount()];
+        for (int i = 0; i < googleMap.markers.Length; i++)
+        {
+            googleMap.markers[i] = new GoogleMapMarker(GoogleMapMarker.GoogleMapMarkerSize.Tiny, GoogleMapColor.white, "_", new GoogleMapLocation[1],GoogleMapMarker.State.none);
+            googleMap.markers[i].locations[0] = new GoogleMapLocation("",-34.61092d, -68.3559d);
+            st = animalsDB.SearchInDBItem(i, animalsDB.dbAnimals, animalsDB.fieldsAnimal);
+            googleMap.markers[i].locations[0].latitude = double.Parse(st[0]);
+            googleMap.markers[i].locations[0].longitude = double.Parse(st[1]);
+        }
+    }
+
+    public void SearchByID()
+    {
+        for (int i = 0; i < mk.Count; i++)
+        {
+            if (mk[i].id == searchField.text)
+            {
+                mk[i].gameObject.transform.GetChild(0).gameObject.SetActive(true);//efecto
+                endMarker = mk[i].transform.localPosition;
+
+                Vector3 dif = contentOriginPos - contentViewMap.transform.localPosition;
+                Debug.Log(dif);
+                endMarker = endMarker - dif;
+
+                endMarker = contentViewMap.transform.localPosition - endMarker;
+                StartLerp();
+                isLerp = true;
+            }
+        }
+
+        cancelSearchBtn.SetActive(true);
+        searchBtn.SetActive(false);
+        searchText = searchField.text;
+        searchField.text = string.Empty;
+        searchField.interactable = false;
+    }
+    public void GetGlobalPosition(Transform pos)
+    {
+        Debug.Log(pos.position + "  /  local: " + pos.localPosition);
+    }
+
+    public void ExitSearch()
+    {
+        string[] st = new string[2];
+        st = animalsDB.SearchItem(animalsDB.dbAnimals, searchText, animalsDB.fieldsAnimal);
+        MarkerController[] mk = FindObjectsOfType<MarkerController>();
+        for (int i = 0; i < mk.Length; i++)
+        {
+            if (mk[i].id == searchText)
+            {
+                mk[i].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+        searchText = string.Empty;
+        cancelSearchBtn.SetActive(false);
+        searchBtn.SetActive(true);
+
+        endMarker = contentOriginPos;
+        StartLerp();
+        isLerp = true;
+
+        searchField.text = string.Empty;
+        searchField.interactable = true;
+    }
+
+    public void ShowHideMenuButtons()
+    {
+        for (int i = 0; i < menuButtons.Length; i++)
+        {
+            menuButtons[i].SetActive(!menuButtons[i].activeInHierarchy);
+        }
+    }
+
+    public void ShowText(string value)
+    {
+        menuButtons[0].GetComponent<Text>().text = value;
+    }
+
+    public void HideText()
+    {
+        menuButtons[0].GetComponent<Text>().text = string.Empty;
+    }
+
+    public void Zoom(bool zoomIn)
+    {
+        if (zoomIn)
+        {
+            if (zoomLevel < 12)
+            {
+                zoomLevel += 1;
+                contentViewMap.transform.localScale *= 1.1f;
+                contentViewMap.transform.localPosition *= 1.1f;
+            } 
+        }
+        else
+        {
+            if (zoomLevel > 1)
+            {
+                zoomLevel -= 1;
+                contentViewMap.transform.localScale *= 0.9f;
+                contentViewMap.transform.localPosition *= 0.9f;
+            }
+        }
+
+        if (zoomLevel >= 11)
+            zoomInBtn.interactable = false;
+
+        else
+            zoomInBtn.interactable = true;
+
+        if (zoomLevel <= 1)
+            zoomOutBtn.interactable = false;
+        else
+            zoomOutBtn.interactable = true;
+    }
+}
